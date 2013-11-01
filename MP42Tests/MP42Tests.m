4e61250dc63a222761fabd13f3e9229ea2a414a2
Damiano Galassi <damiog@gmail.com>
2013-11-01 08:38:09 +0100

    Initial Commit

---
+++ MP42Tests/MP42Tests.m
@@ -0,0 +1,34 @@
+//
+//  MP42Tests.m
+//  MP42Tests
+//
+//  Created by Damiano Galassi on 01/11/13.
+//  Copyright (c) 2013 Damiano Galassi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MP42Tests : XCTestCase
+
+@end
+
+@implementation MP42Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
